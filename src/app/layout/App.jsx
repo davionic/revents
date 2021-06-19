10e01ca034eecf823042b66a6ce835967603438b
Dave <rose.davey@yahoo.com>
2021-06-19 16:07:49 -0400

    nav bar coloring, buttons and left/right page columns

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+    </>
+  );
+}
