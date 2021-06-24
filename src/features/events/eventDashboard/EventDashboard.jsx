b15159ed09dc725460469ed671c2207c61c80fc9
tavishigirotra <81785488+tavishigirotra@users.noreply.github.com>
2021-06-23 21:26:06 -0400

    added some state management to the 'create event' button

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
-import EventForm from '../eventForm/EventForm';
-import EventList from './EventList';
-import {sampleData} from '../../../app/api/sampleData';
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import EventForm from "../eventForm/EventForm";
+import EventList from "./EventList";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard() {
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventList events={sampleData} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <EventForm />
-            </Grid.Column>
-        </Grid>
-    )
+export default function EventDashboard({formOpen, setFormOpen}) {
+  const [events, setEvents] = useState(sampleData);
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} />
+      </Grid.Column>
+      <Grid.Column width={6}>{formOpen && <EventForm setFormOpen={setFormOpen} />}</Grid.Column>
+    </Grid>
+  );
 }
-
